4c90ea41f92d93ac8de402efb43cd70c2b1600e1
Mandisam <mandisam@clc.co.za>
2025-11-10 14:13:31 +0200

    feat: refactoring

--- js/loadpage.js
+++ js/loadpage.js
@@ -34,19 +34,19 @@ $(document).ready(function () {
     $("#store").click(function () {
         playOpenModalSound();
         $('.modal-title').html('<i class="fa fa-shop" aria-hidden="true"></i> Store');
-        loadpage('modal-body', '../html/store.html')
+        loadpage('modal-body', 'html/store.html')
     });
 
     $("#items").click(function () {
         playOpenModalSound();
         $('.modal-title').html('<i class="fa fa-briefcase" aria-hidden="true"></i> Items');
-        loadpage('modal-body', '../html/items.html')
+        loadpage('modal-body', 'html/items.html')
     });
 
     $("#options").click(function () {
         playOpenModalSound();
         $('.modal-title').html('<i class="fa fa-bars" aria-hidden="true"></i> Options');
-        loadpage('modal-body', '../html/options.html')
+        loadpage('modal-body', 'html/options.html')
     });
 
     $(".btn-close").click(function () {
@@ -81,4 +81,4 @@ $(document).ready(function () {
 
 });
 
-loadpage('main-content', 'branches.html')
+loadpage('main-content', 'html/branches.html')
