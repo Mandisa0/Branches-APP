cbd755dee2fe73aef0151ddc1ca9180f8ea81880
Mandisam <mandisam@clc.co.za>
2025-11-28 15:20:52 +0200

    feat: stats in react

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import React, { FC, useEffect } from 'react';
 import Header from './components/Header';
 import Stats from './components/Stats';
 import { initializeAdMob } from './services/admob/admob';
-import { updateStatsInDOM, getStat } from './services/player/statsService';
+import { updateStatsInDOM } from './services/player/statsService';
 import './css/index.css';
 
 export const App: React.FC = () => {
 
   useEffect(() => {
     initializeAdMob();
-    getStat('health');
     updateStatsInDOM();
   }, []);
 
